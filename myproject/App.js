565f886464b45e6e206ac0cc62a298495b1b98f8
Dominik Arment <domarment@gmail.com>
2024-03-11 12:44:58 -0400

    started simple to do app

--- myproject/App.js
+++ myproject/App.js
@@ -1,55 +1,30 @@
 import React, {useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, FlatList, TouchableOpacity } from 'react-native';
+import Header from './components/header';
 
 export default function App() {
-  const [people, setPeople] = useState([
-    
-      { name: "John", id: "1" },
-      { name: "James", id: "2" },
-      { name: "Jack", id: "3" },
-      { name: "Derrick", id: "4" },
-      { name: "Dom", id: "5" },
-      { name: "Greg", id: "6" },
-      { name: "Emma", id: "7" },
-      { name: "Blake", id: "8" },
-      { name: "Kim", id: "9" },
-      { name: "Katie", id: "10" },
-  ])
-
-  const pressHandler = (id)=>{
-    console.log(id)
-    setPeople((prevPeople)=>{
-      return prevPeople.filter(person => person.id != id)
-    })
-  }
-  
+  const [todos, setTodos] = useState([
+    { text: 'buy coffee', key: '1' },
+    { text: 'create an app', key: '2' },
+    { text: 'play on the switch', key: '3' }
+  ]);
 
   return (
     <View style={styles.container}>
-    <FlatList 
-      numColumns={2}
-      keyExtractor={(item)=> item.id}
-      data={people} 
-      renderItem={({ item })=>(
-      <TouchableOpacity onPress={() => pressHandler(item.id)}>
-        <Text style={styles.item}>{item.name}</Text> 
-      </TouchableOpacity>
-      )}
-    />
-    
-    
-    
-    
-    {/* <ScrollView>
-      {people.map((peps)=>{
-        return(
-          <View key={peps.key}>
-            <Text style={styles.item}>{peps.name}</Text>
-          </View>
-        )
-      })}
-      </ScrollView> */}
+      <Header/>
+      <View style={styles.content}>
+        {/* to form */}
+        <View style={styles.list}>
+          <FlatList 
+            data={todos}
+            renderItem={({item})=>(
+              <Text>{item.text}</Text>
+            )}
+          />
+        </View>
+
+      </View>
     </View>
   );
 }
@@ -63,12 +38,11 @@ const styles = StyleSheet.create({
     // alignItems: 'center',
     // justifyContent: 'center',
   },
-  item: {
-    marginTop: 24,
-    padding: 30,
-    backgroundColor: 'pink',
-    fontSize: 24,
-    marginHorizontal: 10,
-    marginTop: 24
+  content: {
+    padding: 40,
+
+  },
+  list: {
+    marginTop: 20
   }
 });
